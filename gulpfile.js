8aa5babc81ccf78b28f9622f8c2b3c0aabecbf5e
Harris Thompson <hjthompson@live.com>
2017-03-19 21:49:08 -0500

    Fix height and add gulp

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const autoprefixer = require('gulp-autoprefixer');
+const csscomb = require('gulp-csscomb');
+const cleanCSS = require('gulp-clean-css');
+
+gulp.task('js', function () {
+  return gulp.src('_include/js/main.js')
+      .pipe(concat('main.min.js'))
+      .pipe(babel({ presets: ['babili'] }))
+      .pipe(gulp.dest('_include/js'));
+});
+
+gulp.task('css', function () {
+  return gulp.src('_include/css/main.css')
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false,
+      }))
+      .pipe(csscomb())
+      .pipe(concat('main.min.css'))
+      .pipe(cleanCSS())
+      .pipe(gulp.dest('_include/css'));
+
+});
+
+gulp.task('default', gulp.series('js', 'css', function (done) {
+  gulp.watch('_include/js/main.js').on('change', gulp.task('js'));
+  gulp.watch('_include/css/main.css').on('change', gulp.task('css'));
+  done();
+}));
